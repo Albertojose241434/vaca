c33f5633562cba5b13d9ab1ce11433f45adb2b58
Albertojose241434 <albertojose241434@gmail.com>
2023-11-09 16:07:24 -0500

    a

---
+++ data/data.js
@@ -0,0 +1,33 @@
+export const data =
+    [
+        {
+            "id": 1,
+            "name": "opcion 1",
+            "image": "./omg/hamburguesa.jpg",
+        },
+        {
+            "id": 2,
+            "name": "2",
+            "image": "/omg/opcion 2.jpg",
+        },
+        {
+            "id": 3,
+            "name": "3",
+            "image": "/omg/opcion3.jpg",
+        },
+        {
+            "id": 4,
+            "name": "4",
+            "image": "/omg/opcion4.jpg",
+        },
+        {
+            "id": 5,
+            "name": "5",
+            "image": "/omg/opcion5.jpg",
+        },
+        {
+            "id": 6,
+            "name": "6",
+            "image": "/omg/opcion6.jpg",
+        },
+    ]
