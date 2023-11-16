dec5ef97b8229a22140cd0cf05a0157783662131
Albertojose241434 <albertojose241434@gmail.com>
2023-11-16 14:15:56 -0500

    e

--- data/data.js
+++ data/data.js
@@ -7,27 +7,27 @@ export const data =
         },
         {
             "id": 2,
-            "name": "2",
+            "name": "opcion 2",
             "image": "/omg/opcion 2.jpg",
         },
         {
             "id": 3,
-            "name": "3",
+            "name": "opcion 3",
             "image": "/omg/opcion3.jpg",
         },
         {
             "id": 4,
-            "name": "4",
+            "name": "opcion 4",
             "image": "/omg/opcion4.jpg",
         },
         {
             "id": 5,
-            "name": "5",
+            "name": "opcion 5",
             "image": "/omg/opcion5.jpg",
         },
         {
             "id": 6,
-            "name": "6",
+            "name": "opcion 6",
             "image": "/omg/opcion6.jpg",
         },
     ]
